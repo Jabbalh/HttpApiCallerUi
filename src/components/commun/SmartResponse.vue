5822937cbfb6957dcd86ce85164d2e3a28a76e00
Jabbalh <nicolas.huet35@gmail.com>
2023-10-06 23:51:45 +0200

    Gestion du changement de theme

--- src/components/commun/SmartResponse.vue
+++ src/components/commun/SmartResponse.vue
@@ -19,39 +19,58 @@
 
 
 import { json } from "@codemirror/lang-json";
-import {computed, onMounted, ref } from "vue";
+import {computed, onMounted, ref, watch} from "vue";
 import { clone } from "lodash";
 //import {EditorView } from "codemirror";
 
 
 import { espresso } from 'thememirror';
 import useTheme from "src/composables/Themes";
-import { EditorView} from "@codemirror/view";
+import { EditorView, lineNumbers,highlightActiveLine} from "@codemirror/view";
 import { oneDark } from '../commun/apiCallerDarkTheme'
-import {EditorState} from "@codemirror/state";
+import {Compartment, EditorState, Extension} from "@codemirror/state";
+import {search} from "@codemirror/search";
 
 const props = defineProps<{response: string}>();
 const currentTheme = useTheme();
 const jsonResponse = clone(JSON.stringify(JSON.parse(props.response), null, "\t"));
 
 const docResponse = ref<Element>();
-const codeMirrorTheme = computed(() => {
-  return currentTheme.isDark() ? oneDark : espresso
+const editor = ref<EditorView>();
+const codeMirrorTheme = computed(() => currentTheme.isDark() ? oneDark : espresso);
+
+const themeConfig = new Compartment()
+
+const extension: Extension = [
+  search(),
+  highlightActiveLine(),
+  lineNumbers(),
+  EditorView.lineWrapping,
+  themeConfig.of(codeMirrorTheme.value),
+  EditorState.readOnly.of(true),
+
+  json()
+];
+
+watch(codeMirrorTheme, () => {
+  console.log("watch theme");
+  editor.value!.dispatch({
+    effects: themeConfig.reconfigure([codeMirrorTheme.value])
+  })
+})
+
+const editoreState = EditorState.create({
+  doc: jsonResponse,
+  extensions: extension
 });
+
 
 
 onMounted(() => {
   if (docResponse.value){
-    new EditorView({
-      state: EditorState.create({
-        doc: jsonResponse,
-        extensions: [
-          EditorView.lineWrapping,
-          codeMirrorTheme.value,
-          json()
-          ]
-      }),
-      parent: docResponse.value,
+    editor.value = new EditorView({
+      state: editoreState,
+      parent: docResponse.value
     })
   }
 
