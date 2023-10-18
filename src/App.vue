11c6bb8cd167e6cca9315e96804daf34cc34932a
Jabbalh <nicolas.huet35@gmail.com>
2023-10-18 22:52:22 +0200

    Correction chargement données

--- src/App.vue
+++ src/App.vue
@@ -3,126 +3,27 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, ref, watch} from 'vue';
-import {useQuasar} from 'quasar';
-import {RestCollection} from 'src/models/model';
-import { uid } from 'quasar'
+import {defineComponent} from 'vue';
 import {useAppStore} from 'stores/appStore';
 import {useEnvStore} from "stores/EnvStore";
-import {LANGUAGE} from "src/models/Constantes";
+import {useLoadDataCollection} from "src/composables/loadLocalStorageData";
 
 export default defineComponent({
   name: 'App',
   setup(){
-    const q$ = useQuasar();
     const appStore = useAppStore();
     const envStore = useEnvStore();
+    const { load, mockCollection, mockEnv } = useLoadDataCollection();
 
-    envStore.$patch({
-      AppEnvironnement: [
-        {
-          name: 'DEV',
-          values: [
-            {
-              key: 'user',
-              value: 'toto dev',
-              active: true
-            },
-            {
-              key:'id',
-              value:'id dev',
-              active: true
-            },
-            {
-              key: 'rootUrl',
-              value: 'http://',
-              active: true
-            }
-          ]
-        },
-        {
-          name: 'PROD',
-          values: [
-            {
-              key: 'user',
-              value: 'toto prod',
-              active: true
-            },
-            {
-              key: 'rootUrl',
-              value: 'http://',
-              active: true
-            }
-          ]
-        }
-      ]
-    })
+    envStore.$patch(mockEnv);
 
-    let mock = ref<RestCollection[]>([{
-      isCollection: true,
-      isLocal: true,
-      id: uid(),
-      name: 'Ma collection',
-      isSaved: true,
-      childs: [],
-      requests: [
-        {
-          id: uid(),
-          name:'Requete 1',
-          method: 'GET',
-          url: 'https://echo.hoppscotch.io',
-          isOpen: true,
-          isSaved: true,
-          parameter: [],
-          header: [],
-          body: {
-            body: '',
-            language: LANGUAGE.nothing
-          },
-          response: undefined
-        },
-        {
-          id: uid(),
-          name:'Requete 2',
-          method: 'POST',
-          url: 'https://test.fr',
-          isOpen: false,
-          isSaved: true,
-          parameter: [],
-          header: [],
-          body: {
-            body: '',
-            language: LANGUAGE.nothing
-          },
-          response: undefined
-        }
-      ]
-    }]);
-
-    if (q$.localStorage.has('REST_STATE')){
-      const state = q$.localStorage.getItem<string>('REST_STATE');
-      if (state){
-        appStore.$patch(JSON.parse(state));
-      }
-
-    } else {
+    if (!load()) {
       appStore.$patch({
-        restCollection: mock,
-      })
+        restCollection: mockCollection,
+      });
     }
 
-    watch(appStore.$state,
-      (state) => {
-        localStorage.setItem('REST_STATE',JSON.stringify(state));
-      },
-      {
-        deep: true
-      }
-    )
 
-    watch(envStore.$state, state => {
-      localStorage.setItem('REST_ENV',JSON.stringify(state));
-    })
   }
 });
 </script>
---
+++ src/composables/loadLocalStorageData.ts
@@ -0,0 +1,126 @@
+import {uid, useQuasar} from "quasar";
+import {IAppStore, useAppStore} from "src/stores/appStore";
+import {useEnvStore} from "src/stores/EnvStore";
+import {RestRequest} from "src/models/model";
+import useRequestUtils from "src/composables/RequestUtils";
+import {LANGUAGE} from "src/models/Constantes";
+import {watch} from 'vue';
+
+export const useLoadDataCollection = function() {
+  const q$ = useQuasar();
+  const appStore = useAppStore();
+  const envStore = useEnvStore();
+
+  watch(appStore.$state,
+    (state) => {
+      localStorage.setItem('REST_STATE',JSON.stringify(state));
+    },
+    {
+      deep: true
+    }
+  )
+
+  watch(envStore.$state, state => {
+    localStorage.setItem('REST_ENV',JSON.stringify(state));
+  },{
+    deep: true
+  });
+
+  const load = () => {
+    if (q$.localStorage.has('REST_STATE')){
+      const state = q$.localStorage.getItem<string>('REST_STATE');
+      if (state){
+        appStore.$patch(parseData(state));
+        return true;
+      }
+    }
+    return false;
+  }
+
+  return {
+    load,
+    mockCollection,
+    mockEnv
+  }
+}
+
+function parseData(state: string){
+  const data: IAppStore = JSON.parse(state);
+  const openCollection:RestRequest[] = [];
+  const requestUtils = useRequestUtils();
+
+  for (const item of data.openedRestRequest){
+    const request = requestUtils.findRequestById(data.restCollection, item.id);
+    if (request) {
+      openCollection.push(request);
+      if (request.id == data.activeRestRequest?.id){
+        data.activeRestRequest = request;
+      }
+    } else {
+      openCollection.push(item);
+    }
+  }
+  data.openedRestRequest = openCollection;
+  return data as any;
+}
+
+const mockCollection = [{
+  isCollection: true,
+  isLocal: true,
+  id: uid(),
+  name: 'Ma collection',
+  isSaved: true,
+  childs: [],
+  requests: [
+    {
+      id: uid(),
+      name:'Requete 1',
+      method: 'GET',
+      url: 'https://echo.hoppscotch.io',
+      isOpen: true,
+      isSaved: true,
+      parameter: [],
+      header: [],
+      body: {
+        body: '',
+        language: LANGUAGE.nothing
+      },
+      response: undefined
+    },
+    {
+      id: uid(),
+      name:'Requete 2',
+      method: 'POST',
+      url: 'https://test.fr',
+      isOpen: false,
+      isSaved: true,
+      parameter: [],
+      header: [],
+      body: {
+        body: '',
+        language: LANGUAGE.nothing
+      },
+      response: undefined
+    }
+  ]
+}];
+
+const mockEnv = {
+  AppEnvironnement: [
+    {
+      name: 'DEV',
+      values: [
+        { key: 'user',  value: 'toto dev',  active: true },
+        { key:'id',  value:'id dev', active: true },
+        { key: 'rootUrl',  value: 'http://',  active: true }
+      ]
+    },
+    {
+      name: 'PROD',
+      values: [
+        { key: 'user',  value: 'toto prod',  active: true },
+        { key: 'rootUrl',  value: 'http://',  active: true }
+      ]
+    }
+  ]
+};
