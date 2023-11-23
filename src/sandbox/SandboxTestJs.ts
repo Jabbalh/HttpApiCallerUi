fb1e1b3ddcef5bd2bbf86197488d66c3b4bb51a1
Jabbalh <nicolas.huet35@gmail.com>
2023-11-23 23:02:28 +0100

    Début d'implémentation de la partie test -> L'exécution fonctionne

--- src/sandbox/SandboxTestJs.ts
+++ src/sandbox/SandboxTestJs.ts
@@ -1,42 +1,71 @@
 import * as qjs from "quickjs-emscripten"
+import {Arena } from 'quickjs-emscripten-sync';
 import {AxiosResponse} from "axios";
 import {useEnvStore} from "stores/EnvStore";
+//import {jsonToVm} from "src/sandbox/MarshalObject";
+//import * as E from "fp-ts/Either";
 
 export const runSandbox = async (script: string, axiosResult: AxiosResponse) => {
-
   const envApp = useEnvStore();
-
   const quickJs = await qjs.getQuickJS();
   const vm = quickJs.newContext();
-  const objectHandle = vm.newObject();
 
-  const setEnvHandle = vm.newFunction('setEnv', (key: qjs.QuickJSHandle, value: qjs.QuickJSHandle) => {
-    const keyValue = vm.dump(key);
-    const valueVakue = vm.dump(value);
-    if (envApp.Current){
-      const env = envApp.Current.values.find(x => x.key == keyValue);
-      if (env){
-        env.value = valueVakue;
-      }
+  const arena = new Arena(vm, { isMarshalable: target => {
+      if (target === window) return false;
+      // complexity is a helper function to detect whether the object is heavy
+      //if (complexity(target, 30) >= 30) return false;
+      return true; // other objects are OK
+    } });
+
+  arena.expose({
+    console: {
+      log: console.log
+    },
+    hac: {
+      setEnv: envApp.addEnvValue,
+      getEnv: envApp.getEnvValue,
+      setGlobalEnv: envApp.setGlovalEnvValue,
+      getGlobalEnv: envApp.getGlobalValue,
+      response: axiosResult
     }
   });
 
-  const responseHandle = vm.newObject()
+  arena.evalCode(script);
+  arena.dispose();
 
-
-  //vm.setProp(objectHandle, 'respopnse',axiosResult);
-
-  vm.setProp(objectHandle,'setEnv',setEnvHandle );
-  setEnvHandle.dispose();
-  vm.setProp(vm.global,'hac',objectHandle);
-  objectHandle.dispose();
-
-  const result = vm.evalCode(script);
-
-  if (result.error) {
-    console.log("Execution failed:", vm.dump(result.error))
-    result.error.dispose()
-  }
+  // const objectHandle = vm.newObject();
+  //
+  // const setEnvHandle = vm.newFunction('setEnv', (key: qjs.QuickJSHandle, value: qjs.QuickJSHandle) => {
+  //   const keyValue = vm.dump(key);
+  //   const valueVakue = vm.dump(value);
+  //   if (envApp.Current){
+  //     const env = envApp.Current.values.find(x => x.key == keyValue);
+  //     if (env){
+  //       env.value = valueVakue;
+  //     }
+  //   }
+  // });
+  //
+  // console.log('axiosResult', axiosResult);
+  // const marshalResponse = jsonToVm(vm, axiosResult);
+  //
+  // if (E.isLeft(marshalResponse)) {
+  //   return E.left(marshalResponse.left);
+  // }
+  // console.log('marshall right ok')
+  // vm.setProp(objectHandle, 'response',marshalResponse.right);
+  // marshalResponse.right.dispose();
+  // vm.setProp(objectHandle,'setEnv',setEnvHandle );
+  // setEnvHandle.dispose();
+  // vm.setProp(vm.global,'hac',objectHandle);
+  // objectHandle.dispose();
+  //
+  // const result = vm.evalCode(script);
+  //
+  // if (result.error) {
+  //   console.log("Execution failed:", vm.dump(result.error))
+  //   result.error.dispose()
+  // }
 
   vm.dispose()
 
--- src/stores/EnvStore.ts
+++ src/stores/EnvStore.ts
@@ -17,5 +17,51 @@ export const useEnvStore = defineStore('env', {
       AppEnvironnement: [],
       Current: null
     }
+  },
+  actions: {
+    /**
+     * Modification ou ajout d'une variable d'environnement
+     * @param key
+     * @param value
+     */
+    addEnvValue: function (key: string, value: string) {
+      if (this.Current) {
+        const env = this.Current.values.find(x => x.key == key);
+        if (env){
+          env.value = value;
+        } else {
+          this.Current.values.push({
+            key: key,
+            value: value,
+            active: true
+          });
+        }
+      }
+    },
+    /**
+     * Renvoi la valeur d'une variable d'environnement
+     * @param key
+     */
+    getEnvValue: function(key: string) {
+      if (this.Current) {
+        return this.Current.values.find(x => x.key == key)?.value ?? "";
+      }
+      return "";
+    },
+    setGlovalEnvValue: function(key: string, value: string) {
+      const env = this.Global.values.find(x => x.key == key);
+      if (env) {
+        env.value = value;
+      } else {
+        this.Global.values.push({
+          key: key,
+          value: value,
+          active: true
+        });
+      }
+    },
+    getGlobalValue: function(key: string){
+      return this.Global.values.find(x => x.key == key)?.value ?? "";
+    }
   }
 });
